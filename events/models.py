1d051bfcb9c43c6c09094c3e54f1f2ae53b58942
AAmir007-code <amiraslonov00@gmail.com>
2023-01-15 01:22:34 -0500

    Connecting Postgresql database

--- events/models.py
+++ events/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    event_image = models.ImageField(upload_to='event_images/')
+    event_text = models.CharField(max_length=300)
--- myawesomeproject/settings.py
+++ myawesomeproject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,14 @@ WSGI_APPLICATION = 'myawesomeproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE' : 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -116,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/awesome_media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
